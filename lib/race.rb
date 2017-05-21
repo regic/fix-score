eedcbe4ad1a8bb336fdd4699ce9793ea3c9b3b0b
kaloyan-dbrdev <kaloyan.yordanov@dbrdev.com>
2017-05-21 03:58:03 +0300

    Make the tests pass
    A nive solution is implemented - the points and the bonus points are set
    as class constants, then we loop only through the first `points.length`
    guesses (it is pointless to check the others), then we check if the guess
    mathes a winner and assign the points to `result`, if not - check if the
    guess is included in the winners (apparently in the wrong place) and if
    it is - add bonus point(s).

--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,19 @@
 class Race
-  def self.score(guesses, winners)
+  POINTS = [15, 10, 5, 3, 1].freeze
+  BONUS = 1
 
+  def self.score(guesses, winners)
+    points = 0
+
+    guesses.first(POINTS.length).each_with_index do |guess, index|
+      if guess == winners.at(index)
+        points += POINTS.at(index)
+      elsif winners.include? guess
+        points += BONUS
+      end
+    end
+
+    points
   end
 end
+
